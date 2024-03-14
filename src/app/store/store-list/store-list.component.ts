c4fa36ad965740422f2444ae1cd46b10b266fc8e
Sebastian <sebastian.hanssen@outlook.com>
2024-03-14 11:35:09 +0100

    finish sort extensions

--- src/app/store/store-list/store-list.component.ts
+++ src/app/store/store-list/store-list.component.ts
@@ -1,13 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { StoreService } from '../../services/store.service';
+import { Item } from '../../models/item';
 
 @Component({
   selector: 'app-store-list',
   templateUrl: './store-list.component.html',
   styleUrls: ['./store-list.component.css']
 })
-export class StoreListComponent {
+export class StoreListComponent implements OnInit {
+  items: Item[] = [];
+
   constructor(private readonly storeService: StoreService) {}
 
-  items = this.storeService.storeItems;
+  ngOnInit() {
+    this.sortDefault();
+  }
+
+  sortPriceAscending() {
+    this.items.sort((a, b) => a.price - b.price);
+  }
+
+  sortPriceDescending() {
+    this.items.sort((a, b) => b.price - a.price);
+  }
+
+  sortName() {
+    this.items.sort((a, b) => a.name.localeCompare(b.name));
+  }
+
+  async sortDefault() {
+    this.items = await this.storeService.storeItems;
+  }
 }
